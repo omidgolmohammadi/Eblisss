2b0101751f2f740fa9c5f953920040b4a452f025
TitanTeamGit <TitanTeam@email.com>
2016-04-08 07:40:34 -0700

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -3,88 +3,88 @@ do
 function run(msg, matches)
   local help = [[Self-Bot Commands
 
-●#bot on
+●bot on
 فعال کردن بوت در یک گروه خواص
 
-●#bot off
+●bot off
 غیر فعال کردن بوت در یک گروه خواض
 
-●#plugins
+●plugins
 مشاهده لیست پلاگین ها
 
-●#plugins enable (plugin name)
+●plugins enable (plugin name)
 فعال کردن پلاگینی با نام (plugin name)
 
-●#plugins disable (plugin name)
+●plugins disable (plugin name)
 غیر فعال کردن پلاگینی با نام (plugin name)
 
-●#plugins reload
+●plugins reload
 آپدیت کردن لیست پلاگین ها
 
-●#plugins disable (name) gp
+●plugins disable (name) gp
 غیر فعال کردن پلاگین (name) در گروه مورد نظر
 
-●#plugins disable (name) gp
+●plugins disable (name) gp
 فعال کردن پلاگین (name) در گروه مورد نظر
 
-●#addplug (text) (name)
+●addplug (text) (name)
 اضافه کردن پلاگینی به محتوای (text)و نام (name) به لیست پلاگین 
 
-●#getplug (name)
+●getplug (name)
 ارسال پلاگین با نام (name)
 
-●#setname (name)
+●setname (name)
 تغیر نام گروه به (name)
 
-●#link 
+●link 
 دریافت لینک گروه در پیوی
 
-●#newlink
+●newlink
 ساخت لینک جدید
 
-●#tosuper
+●tosuper
 تبدیل گروه معمولی به سوپر گوه
 
-●#setdes (text)
+●setdes (text)
 تغیر دسکریپشن گروه به (text)
 
-●#rmv @username (by reply)
+●rmv @username (by reply)
 اخراج فردی با آیدی @username (حتی با ریپلای)
 
-●#add @username (by reply)
+●inv @username (by reply)
 ادد کردن فردی با آیدی @username به گروه (حتی با ریپلای)
 
-●#id @username (by reply)
+●id @username (by reply)
 دریافت آیدی عددی فردی با آیدی @username (حتی با ریپلای)
 
-●#gid
+●gid
 دریافت آیدی گروه
 
-●#google (text)
+●google (text)
 جستجو (text) در گوگل
 
-●#webshot (http://google.com)
+●webshot (http://google.com)
 دریافت اسکرین شات از سایت گوگل
 
-●#voice (text)
+●voice (text)
 تبدیل (text) به صدا
 
-●#insta (insta id)
+●insta (insta id)
 دریافت اطلاعات اینستاگرام فردی با آیدی (intsa id)
 
-●#insta (post link)
+●insta (post link)
 دریافت اطلاعات پستی در اینستاگرام با لینک (post link)
 
-●#set (text1) (text2)
+●set (text1) (text2)
 سیو شدن (text2) در جواب (text)
 
-●#get (text1)
+●get (text1)
 ارسال متن سیو شده برای (text1) یعنی (text2)
 
-●#weather (city)
+●weather (city)
 دریافت اطلاعات آب و هوای شهر (city)
 
-●#sticker (text)
+●sticker (text)
 تبدیل (text) به استیکر
 
 .......................................
